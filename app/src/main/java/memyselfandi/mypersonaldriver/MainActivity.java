fb42576f082978dc5d14969fe914d7276a5b5d10
Lefoulon, Ludovic <Ludovic.Lefoulon@aptushealth.com>
2017-09-17 17:53:13 +0200

    étape 1 add mapbox sdk

--- app/src/main/java/memyselfandi/mypersonaldriver/MainActivity.java
+++ app/src/main/java/memyselfandi/mypersonaldriver/MainActivity.java
@@ -1,13 +1,68 @@
 package memyselfandi.mypersonaldriver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mapView)
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiemFtYnJhbCIsImEiOiJjajdvd25vNHYzZWZwMzdxbWxxY216dTZtIn0.RTC8b6Z5o6pcZzH8B9cU6A");
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapView.onLowMemory();
+        super.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
